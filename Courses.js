c8536e292a6bf3dffe3cedd12452c61bb4e8be73
aaronglenore <91707421+aaronglenore@users.noreply.github.com>
2023-03-28 19:41:19 -0400

    Add files via upload

---
+++ Courses.js
@@ -0,0 +1,16 @@
+class Courses
+{
+    constructor(Name, Department, Code, Instructor, Credits, ID)
+    {
+        this.Name = Name;
+        this.Department = Department;
+        this.Code = Code;
+        this.Instructor = Instructor;
+        this.Credits = Credits;
+        this.ID = ID;
+    }
+}
+
+
+
+export default Courses;
---
+++ Student.js
@@ -0,0 +1,15 @@
+class Student
+{
+    constructor(ID, Name, tMajor, Standing, Allowed, courses)
+    {
+        this.ID = ID;
+        this.Name = Name;
+        this.Major = tMajor;
+        this.Standing = Standing;
+        this.Allowed = Allowed;
+        this.courses = courses;
+    }
+}
+
+
+export default Student;
